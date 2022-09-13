7ebb3f76b69d7c7ee016b747b6b727dee17f45be
admin <omegagamma314@gmail.com>
2022-09-13 14:26:01 -0700

    2

--- main.cpp
+++ main.cpp
@@ -5,19 +5,34 @@
 using namespace sf;
 int main()
 {
+    
     VideoMode win(1280,960);
     
     Window window(win,"Chaos Game",Style::Fullscreen);
-
+    Event event;
+    Mouse mouse1;
+    Keyboard keyboard1;
     std::vector<int> points; 
-    while (window.isOpen())
+    while (window.pollEvent(event))
     {
-            if(Mouse::isButtonPressed(Mouse::Left) && points.size() < 6)
+            if(event.type == Event::MouseLeft && points.size() < 6)
             {
-            points.push_back(Mouse::getPosition(window).x);
-            points.push_back(Mouse::getPosition(window).y);
+            points.push_back(mouse1.getPosition(window).x);
+            points.push_back(mouse1.getPosition(window).y);
             }
-        }
+
+        
+        
+     
+   
     
+    }
+    if(keyboard1.Escape)
+        {
+            window.close();
+        }
+    for(int i = 0; i < 6; i++)
+    {
+        std::cout << points[i] << endl;
     }
 }
